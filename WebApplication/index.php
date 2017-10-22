6bf431c68b9dca439f5316ae8b805ea7f1d2e7f5
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-10-23 01:53:51 +0530

    EER update, Dashboard, New employee partially completed.

--- WebApplication/index.php
+++ WebApplication/index.php
@@ -156,7 +156,7 @@ switch (ENVIRONMENT)
 	// $routing['directory'] = '';
 
 	// The controller class file name.  Example:  mycontroller
-	 $routing['controller'] = 'Dashboard';
+	 $routing['controller'] = 'NewEmployee';
 
 	// The controller function you wish to be called.
 	// $routing['function']	= '';
