65d83ddf6530fd4f6fe9c19ba9f125a25c38cbd5
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-10-22 08:58:48 +0530

    Common links fixed.

--- WebApplication/index.php
+++ WebApplication/index.php
@@ -156,7 +156,7 @@ switch (ENVIRONMENT)
 	// $routing['directory'] = '';
 
 	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
+	 $routing['controller'] = 'Dashboard';
 
 	// The controller function you wish to be called.
 	// $routing['function']	= '';
