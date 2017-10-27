038a2f81cb7215af3467e5fd15d49026ba229f94
Dileka Jayarathna <dilprabuddhi@gmail.com>
2017-10-27 09:00:32 +0530

    Login and Inbox Frontend

--- WebApplication/application/controllers/Login.php
+++ WebApplication/application/controllers/Login.php
@@ -7,4 +7,23 @@ class Login extends CI_Controller
     {
         $this->load->view('login');
     }
+
+    public  function login(){
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+        $this->form_validation->set_rules('password','Password','required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('login.php');
+
+        } else {
+            $enc_password = md5($this->input->post('password'));
+            $this->employee_login->login($enc_password);
+            //set message
+            $this->session->set_flashdata('User logged', 'User is now logged successfully!');
+
+            redirect('login/login');
+        }
+
+    }
+
 }
